2f34aca532a5f1f9f70dbed61f5ac8745ab231e9
Jeffrey-Cawiezell <47756719+Jeffrey-Cawiezell@users.noreply.github.com>
2019-02-21 10:40:28 -0600

    Create Loop.java

---
+++ Loop.java
@@ -0,0 +1,13 @@
+public class Loop{
+	public static void main (String [] args){
+		int counter = 0;
+		while(counter<20){
+			if((counter%2)==1){
+				System.out.println(counter);
+
+			}
+			counter = counter + 1;
+			
+		}
+	}
+}
